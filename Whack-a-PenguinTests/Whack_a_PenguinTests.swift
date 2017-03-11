7b167d54d06802aa2d677c7fe56740c3f9556ec5
FranziskaLuzia <franziskakammerl@Franziskas-MacBook-Pro.local>
2017-03-11 11:49:53 -0500

    Initial Commit

---
+++ Whack-a-PenguinTests/Whack_a_PenguinTests.swift
@@ -0,0 +1,36 @@
+//
+//  Whack_a_PenguinTests.swift
+//  Whack-a-PenguinTests
+//
+//  Created by Franziska Kammerl on 3/11/17.
+//  Copyright © 2017 Franziska Kammerl. All rights reserved.
+//
+
+import XCTest
+@testable import Whack_a_Penguin
+
+class Whack_a_PenguinTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
